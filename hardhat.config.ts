9859d32bfdf146119f9e0d4491cf442a171d766f
le.huy.du <SUN-ASTERISK\le.huy.du@b122952-pc.sun-asterisk.com>
2022-03-24 21:30:59 +0700

    config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,9 +25,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  defaultNetwork: "localhost",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
@@ -37,7 +43,7 @@ const config: HardhatUserConfig = {
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
   },
 };
 
